e4f96b995ce1e6269255411390b596373d8dd1b2
Paneenutella0654 <104584785+Paneenutella0654@users.noreply.github.com>
2022-09-30 01:20:30 +0200

    Update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,6 +4,7 @@ import logo from "../assets/img/logo.svg";
 import navIcon1 from "../assets/img/nav-icon1.svg";
 import navIcon2 from "../assets/img/nav-icon2.svg";
 import navIcon3 from "../assets/img/nav-icon3.svg";
+import navIcon4 from '../assets/img/nav-icon4.svg';
 
 export const Footer = () => {
   return (
@@ -17,13 +18,16 @@ export const Footer = () => {
           <Col size={12} sm={6} className="text-center text-sm-end">
             <div className="social-icon">
               <a href="#">
-                <img src={navIcon1} alt="Icon" />
+                <img src={navIcon4} alt="Icon" />
               </a>
               <a href="#">
                 <img src={navIcon2} alt="Icon" />
               </a>
               <a href="#">
                 <img src={navIcon3} alt="Icon" />
+              </a>
+              <a href="#">
+                <img src={navIcon1} alt="Icon" />
               </a>
             </div>
             <p>Copyright 2022. All Rights Reserved</p>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,6 +4,7 @@ import logo from '../assets/img/logo.svg';
 import navIcon1 from '../assets/img/nav-icon1.svg';
 import navIcon2 from '../assets/img/nav-icon2.svg';
 import navIcon3 from '../assets/img/nav-icon3.svg';
+import navIcon4 from '../assets/img/nav-icon4.svg';
 import { HashLink } from 'react-router-hash-link';
 import {
   BrowserRouter as Router
@@ -50,12 +51,13 @@ export const NavBar = () => {
             </Nav>
             <span className="navbar-text">
               <div className="social-icon">
-                <a href="#"><img src={navIcon1} alt="" /></a>
+                <a href="#"><img src={navIcon4} alt="" /></a>
                 <a href="#"><img src={navIcon2} alt="" /></a>
                 <a href="#"><img src={navIcon3} alt="" /></a>
+                <a href="#"><img src={navIcon1} alt="" /></a>
               </div>
               <HashLink to='#connect'>
-                <button className="vvd"><span>Let’s Connect</span></button>
+                <button className="vvd"><span>Contattami</span></button>
               </HashLink>
             </span>
           </Navbar.Collapse>
